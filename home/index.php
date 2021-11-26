cff5ac4943cee931496440a4b6ae0e0f389bb006
henryust12 <henryust12@gmail.com>
2021-11-26 18:54:26 +0900

    updated homepage

--- home/index.php
+++ home/index.php
@@ -1,9 +1,12 @@
 <h1>Welcome To ShimaTech ShortURL</h1>
 <hr>
-click <a href="./generate">here</a> to generate your link
+click <a href="./generate">here</a> to generate your link <br>
+<br>
+click <a href="./custom">here</a> to custom your link <br>
+
 <br><br><br>
 <hr>
 <p style="text-align: center; font-size: x-large;">
     Made by <a href="http://sshort.tk/shima">ShimaTech.Net</a>&#8195;
-    <a href="https://github.com/henryust12/ShimaTech_ShortUrl/blob/master/README.md">GitHub</a>
+    <a href="https://github.com/henryust12/ShimaTech_ShortUrl">GitHub</a>
 </p>
